f27f06321121127858227468dc516790ac85eed5
Ivan Diliso <diliso.ivan@gmail.com>
2019-05-17 11:07:48 +0200

    Completata esercitazione 1

---
+++ src/ContinuousAttribute.java
@@ -0,0 +1,15 @@
+
+class ContinuousAttribute extends Attribute {
+    private double max;
+    private double min;
+
+    public ContinuousAttribute(String name, int index, double min, double max) {
+        super(name, index);
+        this.max = max;
+        this.min = min;
+    }
+
+    double getScaledValue(double v){
+        return ((v-min)/(max-min));
+    }
+}
---
+++ src/DiscreteAttribute.java
@@ -0,0 +1,17 @@
+
+class DiscreteAttribute extends Attribute {
+    private String[] values;
+
+    public DiscreteAttribute(String name, int index, String[] values) {
+        super(name, index);
+        this.values = values;
+    }
+
+    public int getNumberOfDistinctValues() {
+        return values.length;
+    }
+
+    public String getValue(int i) {
+        return values[i];
+    }
+}
